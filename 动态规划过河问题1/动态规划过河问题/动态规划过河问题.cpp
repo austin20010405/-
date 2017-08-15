03a54ad2eedcbf7d47078a8f250eea614a0804bd
austin20010405 <30500747+austin20010405@users.noreply.github.com>
2017-08-15 22:55:35 +0800

    新

--- 动态规划过河问题1/动态规划过河问题/动态规划过河问题.cpp
+++ 动态规划过河问题1/动态规划过河问题/动态规划过河问题.cpp
@@ -6,20 +6,22 @@
 #include<cstdlib>
 
 int a, b[101], e,temp;
-int han(int i)
+void han(int i)
 {
-	if (i = 1)
+	if (i ==1)
 	{
 		e += b[1];
-		return;
+		return ;
 	}
-	if (i = 2)
+	if (i ==2)
 	{
 		e += b[2];
+		return ;
 	}
-	if (i = 3)
+	if (i ==3)
 	{
 		e += b[1] + b[2] + b[3];
+		return ;
 	}
 	if (b[2] + b[1] + b[i] + b[2]<b[1] + b[i] + b[i - 1] + b[1])
 	{
@@ -35,15 +37,20 @@ int han(int i)
 
 int main()
 {
-
+	e = 0;
+	b[0] = 0;
 	scanf("%d", &a);
+	if (a > 100000)
+	{
+		return -1;
+	}
 	for (int c = 1; c <= a; c++)
 	{
-		scanf("%d", &b[c]);
+		scanf("%d",&b[c]);
 	}
 	for (int i = 1; i <= a; i++)
 	{
-		for (int k = 1; k <= a; k++)
+		for (int k = 1; k <a; k++)
 		{
 			if (b[k] > b[k + 1])
 			{
@@ -54,6 +61,7 @@ int main()
 		}
 	}
 	han(a);
-	printf("%d", &e);
+	printf("%d",e);
+	system("pause");
 	return 0;
 }
